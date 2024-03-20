392fa253a5ddd0c9d7b15684c6af6d4991b04104
Aislu Kharisova <arslanova1996ais@list.ru>
2024-03-20 16:02:47 +0300

    Решение задач.

--- ArrayString/unique.js
+++ ArrayString/unique.js
@@ -2,16 +2,16 @@
 // Уникальность всех символов в строке
 // Напишите функцию, которая определяет уникальны ли все символы в строке. Регистр должен учитываться: ‘a’ и ‘A’ разные символы.
 
-// function isUnique(string) {
-//     Пробегаемся по строке
-//     for (let i = 0; i < string.length; i++) {
-//         методом lastIndexOf ищем индекс последнего элемента
-//         if (string.lastIndexOf(string[i]) !== i) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+function isUnique(string) {
+    //Пробегаемся по строке
+    for (let i = 0; i < string.length; i++) {
+       // методом lastIndexOf ищем индекс последнего элемента
+        if (string.lastIndexOf(string[i]) !== i) {
+            return false;
+        }
+    }
+    return true;
+}
 
 // function isUnique(string) {
 //     записываем в коллекцию только уникальные значения строки, получаем количество уникальных значений методом size
@@ -19,8 +19,8 @@
 //     return new Set(string).size === string.length
 // }
 
-// console.log(isUnique('abcdef'));
-// console.log(isUnique('1234567'));
-// console.log(isUnique('abcABC'));
-// console.log(isUnique('abcadef'));
+console.log(isUnique('abcdef'));
+console.log(isUnique('1234567'));
+console.log(isUnique('abcABC'));
+console.log(isUnique('abcadef'));
 
