6f7e584d9a8c9fbdd659eb00806c5cb64f297e46
Aislu Kharisova <arslanova1996ais@list.ru>
2024-03-22 11:19:33 +0300

    Решение задач.  Напишите функцию groupBy.

--- Algorithms/GroupBy.js
+++ Algorithms/GroupBy.js
@@ -1,16 +1,25 @@
-// напишите функцию groupBy
+// Напишите функцию groupBy
 function groupBy(array, fn) {
-    const res = {};
-    for (let i = 0; i < array.length; i++) {
-        const current = array[i];
-
+    // const res = {};
+    // for (let i = 0; i < array.length; i++) {
+    //     const current = array[i];
+    //
+    //     const key = typeof fn === 'function' ? fn(current) : current[fn];
+    //     if (!res[key]) {
+    //         res[key] = [];
+    //     }
+    //     res[key].push(current);
+    // }
+    // return res;
+// const res = {};
+    return array.reduce((res, current) => {
         const key = typeof fn === 'function' ? fn(current) : current[fn];
         if (!res[key]) {
             res[key] = [];
         }
         res[key].push(current);
-    }
-    return res;
+        return res;
+    }, {})
 }
 
 console.log(groupBy([6.1, 4.2, 6.3], Math.floor)); //{'4': [4.2], '6': [6.1, 6.3]}
