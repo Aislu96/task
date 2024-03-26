4cefa3e81fe56ddba2e47405eb6a440fe793bd7d
Aislu Kharisova <arslanova1996ais@list.ru>
2024-03-26 10:22:24 +0300

    Решение задач. Порядок стека

---
+++ Codewars/timePromies.js
@@ -0,0 +1,12 @@
+console.log(1);
+setTimeout(() => {
+    console.log(2)
+}, 0);
+const myPromise = new Promise((resolve, reject) => {
+    console.log(3);
+    resolve(4);
+}).then((value) => console.log(value));
+
+console.log(5);
+
+// 1, 3, 5, 4, 2
