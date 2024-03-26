f40f17c03b5120832b376f84338dce4d02bd1f88
Aislu Kharisova <arslanova1996ais@list.ru>
2024-03-26 16:33:24 +0300

    Решение задач. Параллельные функции

--- Codewars/parallel.js
+++ Codewars/parallel.js
@@ -27,3 +27,31 @@ async function parallel(arrFunction, showResultFunc) {
     // results.then(data => showResultFunc(data));
     return showResultFunc(results);
 }
+
+
+// async function parallel(arr, cb) {
+//     arr = arr.map(e => new Promise((resolve) => {
+//             const func = e(resolve);
+//             func === undefined ? func : resolve(e());
+//         })
+//     );
+//
+//     let result = await Promise.all(arr);
+//     cb(result);
+// }
+
+// parallel([
+//     function (resolve) {
+//         setTimeout(function () {
+//             resolve(10);
+//         }, 50);
+//     }, function () {
+//         return 5;
+//     }, function (resolve) {
+//         setTimeout(function () {
+//             resolve(0)
+//         }, 10)
+//     }
+// ], function (results) {
+//     console.log(results);
+// })
