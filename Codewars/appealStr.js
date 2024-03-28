1f84c092c22bee72e733389a3ac85d453002eb2a
Aislu Kharisova <arslanova1996ais@list.ru>
2024-03-28 15:09:06 +0300

    Решение задач.

--- Codewars/appealStr.js
+++ Codewars/appealStr.js
@@ -1,5 +1,16 @@
-function appealStr(str){
-    return  str.replaceAll('-', '');
-}
+// function appealStr(str){
+//     return str.replaceAll('-', '');
+// }
+
+
+// function appealStr(str){
+//     for (let i = 0; i < str.length; i++){
+//         if (str[i] === '-'){
+//             str = str.slice(0, i) + str.slice(i+1, str.length)
+//         }
+//     }
+//     return str
+// }
+
 
 console.log(appealStr('aaaaa-bbbb-cc-ddd-eeee'))
