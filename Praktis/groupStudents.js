9c8d6fa601c1ed8a9eb0932f6a5fb81331604933
Aislu Kharisova <arslanova1996ais@list.ru>
2024-04-18 15:12:35 +0300

    Решение задач.

---
+++ Praktis/groupStudents.js
@@ -0,0 +1,22 @@
+const students = [
+    {name: 'alex', age: 20},
+    {name: 'mike', age: 24},
+    {name: 'masha', age: 20},
+    {name: 'stas', age: 18}
+]
+
+
+function groupStudents(students){
+    let stud = {};
+    students.forEach(s => {
+        if(!stud[s.age]) {
+            stud[s.age] = [s]
+        } else {
+            stud[s.age].push(s)
+        }
+
+    })
+    return stud;
+}
+
+console.log(groupStudents(students))
