a07ea244c3dc5f7576575981405f62b6b06bf837
aislusha96 <arslanova1996ais@list.ru>
2024-08-02 18:55:09 +0300

    new

---
+++ LeetCode/Valid Parentheses.js
@@ -0,0 +1,24 @@
+var isValid = function(s) {
+    let arr = [];
+    if(s.length < 2) return false;
+    if(s[0] === ")" || s[0] === "]" || s[0] === "}") return false;
+    for( let i = 0; i < s.length; i++){
+        let num = arr.length -1;
+        if(s[i] === "(" || s[i] === "[" || s[i] === "{") {
+            arr.push(s[i])
+        } else if (arr[num] === '(' && s[i] === ')') {
+            arr.pop();
+        } else if (arr[num] === '{' && s[i] === '}') {
+            arr.pop();
+        } else if (arr[num] === '[' && s[i] === ']') {
+            arr.pop();
+        } else {
+            return false;
+        }
+    }
+    return arr.length === 0
+};
+
+isValid("()");
+isValid("()[]{}");
+isValid("(]")
